62368f4726206ab594c23aa256213a0de2f5c46c
jithesh944 <jithesh944@gmail.com>
2021-02-24 22:52:21 +0900

    user login, logout and profile
    created a new html page for profile

--- django_prj/urls.py
+++ django_prj/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 from users import views as user_views
 from django.conf.urls import url 
 
@@ -23,5 +24,8 @@ from django.conf.urls import url
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
-    path('register/',user_views.register,name='register')  
+    path('register/',user_views.register,name='register'),
+    path('profile/',user_views.profile,name='profile'),
+    path('login/',auth_views.LoginView.as_view(template_name='users/login.html') ,name='login'),
+    path('logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'),name='logout'),
 ]
