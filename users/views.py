06e277c3cb0dd17e5a8e8a38061029b42d7a4bfc
jithesh944 <jithesh944@gmail.com>
2021-02-24 15:49:47 +0900

    created a new branch

--- users/views.py
+++ users/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render,redirect
-# from django.contrib.auth.forms import UserCreationForm, UsernameField
 from django.contrib  import messages
 from .forms import UserRegisterForm 
 # Create your views here.
